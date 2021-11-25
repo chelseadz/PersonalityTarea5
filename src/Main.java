8b764edc043e48315e078468fdbd92f5ef86d571
DIEGO TORRES GONZALEZ <a220206322@unison.mx>
2021-11-24 17:22:03 -0700

    Add methods to TestResults

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args){
+
+        TestResults Chelsea = new TestResults(
+                "Chelsea",
+                "BABAAAABAAAAAAABAAAABBAAAAAABAAAABABAABAAABABABAABAAAAAABAAAAAAB" +
+                        "AAAAAA");
+
+        System.out.println(Chelsea);
         
     }
 }
